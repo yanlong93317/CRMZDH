2190117ee5ed8465f57914b0f712ffc8fd8e37d9
tangli <2551299698@qq.com>
2020-12-29 10:19:28 +0800

    修改test_notice文件

---
+++ page/clue_page.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from page.base_page import BasePage
+
+source_locator=(By.LINK_TEXT,'线索')
+addsource_locator=(By.LINK_TEXT,'新建线索')
+
