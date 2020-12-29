dfc764973f42878ccef90433aa7591ee1c1f0d3a
YHC <1552170761@qq.com>
2020-12-29 19:58:41 +0800

    提交删除后的所有内容

--- unit/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time : 2020/12/9 15:01
-# @Author : yanlong
-# @Email : tangli@tmail.com
-# @File : __init__.py.py
-# @Project : ZIDONG
--- unit/logincsv.py
+++
@@ -1,9 +0,0 @@
-import csv
-def read_csv():
-    with open(file="lgoin.csv") as f:
-        data=csv.reader(f)
-        set_list=[]
-        for user in data:
-            set_list.append(user)
-        return set_list[1:]
-# print(read_csv())
