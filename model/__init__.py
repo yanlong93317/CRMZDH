04239577adfad06156b17431b205fee976d47abd
yanlong93317 <71001374+yanlong93317@users.noreply.github.com>
2020-12-29 19:12:30 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,14 +1,1 @@
-# -*- coding: utf-8 -*-
-
-# @Time : 2020/12/11 15:04
-# @Author : yanlong
-# @Email : tangli@tmail.com
-# @File : __init__.py.py
-# @Project : ZDH
-
-# @Time : 2020/12/26 22:32
-# @Author : zj12345
-# @Email : 374680231@qq.com
-# @File : __init__.py.py
-# @Project : CrmZDH.test
 
