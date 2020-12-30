b6aa78abba1ca8e69211e100ce40c32c71b13739
tangli <2551299698@qq.com>
2020-12-30 12:01:15 +0800

    修改文件

---
+++ cases/addnotice_test.py
@@ -0,0 +1,167 @@
+import unittest
+from page.base_page import BasePage
+from model.browser import BroswerModel
+from page.login_page import LoginPage
+from page.home_page import HomePage
+from time import sleep
+from page.addnotice_page import AddNotice
+from page.noticedetail_page import DetailNotice
+from page.editnotice_page import EditNotice
+
+class AddMailTest(unittest.TestCase):
+    def setUp(self) -> None:
+        print('测试')
+
+    @unittest.skip
+    def test_addmail(self):
+        liulan=BroswerModel()      #实例化一个浏览器类
+        self.driver=liulan.broswer_chrome()       #driver重命名
+        BP=BasePage(driver=self.driver)       #引入一个driver
+        BP.open()
+
+        DL=LoginPage(driver=self.driver)
+        username,password='tangli','admin123456'
+        DL.login(username,password)
+
+        HP=HomePage(driver=self.driver)
+        HP.tangliname()
+        HP.notice()
+
+        AN=AddNotice(driver=self.driver)
+        AN.add_notice()
+
+        DN=DetailNotice(driver=self.driver)
+        btcontent='添加公告测试'
+        DN.title_notice(btcontent)
+        sleep(1)
+        nrcontent='添加公告测试内容'
+        DN.nrcontent_notice(nrcontent)
+        sleep(1)
+        DN.save_notice()
+        sleep(1)
+        AN.getadd_notice()
+        sleep(5)
+        expect = '公告添加成功'
+        actual = AN.getadd_notice().text
+        self.assertIn(expect, actual, msg='添加公告失败')
+
+
+    @unittest.skip
+    def test_seenotice(self):
+        liulan = BroswerModel()  # 实例化一个浏览器类
+        self.driver = liulan.broswer_chrome()  # driver重命名
+        BP = BasePage(driver=self.driver)  # 引入一个driver
+        BP.open()
+
+        DL = LoginPage(driver=self.driver)
+        username, password = 'tangli', 'admin123456'
+        DL.login(username, password)
+
+        HP = HomePage(driver=self.driver)
+        HP.tangliname()
+        HP.notice()
+
+        AN = AddNotice(driver=self.driver)
+        AN.see_notice()
+
+        EN=EditNotice(driver=self.driver)
+        EN.getnrcontent_notice()
+        sleep(5)
+        expect = '测试添加公告内容'
+        actual = EN.getnrcontent_notice().text
+        self.assertIn(expect, actual, msg='查看公告失败')
+
+    @unittest.skip
+    def test_editnotice(self):
+        liulan = BroswerModel()  # 实例化一个浏览器类
+        self.driver = liulan.broswer_chrome()  # driver重命名
+        BP = BasePage(driver=self.driver)  # 引入一个driver
+        BP.open()
+
+        DL = LoginPage(driver=self.driver)
+        username, password = 'tangli', 'admin123456'
+        DL.login(username, password)
+
+        HP = HomePage(driver=self.driver)
+        HP.tangliname()
+        HP.notice()
+
+        AN = AddNotice(driver=self.driver)
+        AN.see_notice()
+
+        EN = EditNotice(driver=self.driver)
+        EN.edit_notice()
+        DN=DetailNotice(driver=self.driver)
+        alcontent='加一句'
+        DN.alterbiaoti_notice(alcontent)
+        DN.save_notice()
+        sleep(5)
+        expect = '公告保存成功'
+        actual = AN.getalcontent_notice().text
+        self.assertIn(expect, actual, msg='修改保存公告失败')
+
+
+    @unittest.skip
+    def test_closenotice(self):
+        liulan = BroswerModel()  # 实例化一个浏览器类
+        self.driver = liulan.broswer_chrome()  # driver重命名
+        BP = BasePage(driver=self.driver)  # 引入一个driver
+        BP.open()
+
+        DL = LoginPage(driver=self.driver)
+        username, password = 'tangli', 'admin123456'
+        DL.login(username, password)
+
+        HP = HomePage(driver=self.driver)
+        HP.tangliname()
+        HP.notice()
+        sleep(1)
+
+        AN = AddNotice(driver=self.driver)
+        AN.tingyong_notice()
+        sleep(3)
+        AN.gettingyong_notice()
+        sleep(5)
+        expect = '修改成功，已停用'
+        actual = AN.gettingyong_notice().text
+        self.assertIn(expect, actual, msg='停用公告失败')
+
+
+    def test_deletenotice(self):
+        liulan = BroswerModel()  # 实例化一个浏览器类
+        self.driver = liulan.broswer_chrome()  # driver重命名
+        BP = BasePage(driver=self.driver)  # 引入一个driver
+        BP.open()
+
+        DL = LoginPage(driver=self.driver)
+        username, password = 'tangli', 'admin123456'
+        DL.login(username, password)
+
+        HP = HomePage(driver=self.driver)
+        HP.tangliname()
+        HP.notice()
+        sleep(1)
+
+        AN = AddNotice(driver=self.driver)
+        AN.checkbox_notice()
+        AN.delete_notice()
+        sleep(3)
+        sercontent='添加公告测试'
+        AN.serchcontent_notice(sercontent)
+        sleep(1)
+        AN.serch_notice()
+        sleep(1)
+        AN.getdelete_notice()
+        sleep(5)
+        expect = '暂无数据'
+        actual = AN.getdelete_notice().text
+        print(actual)
+        self.assertIn(expect, actual, msg='删除公告失败')
+
+    def tearDown(self) -> None:
+        print('成功')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
---
+++ page/addnotice_page.py
@@ -0,0 +1,87 @@
+from selenium.webdriver.common.by import By
+from page.base_page import BasePage
+
+class AddNotice(BasePage):
+    add_notice_locator = (By.CSS_SELECTOR, 'body > div.container > div.row > div:nth-child(1) > div.pull-right > a')
+    getadd_notice_locator = (By.CSS_SELECTOR, 'body > div.container > div.alert.alert-success')
+    see_notice_locator = (By.CSS_SELECTOR, '#form1 > table > tbody > tr:nth-child(1) > td:nth-child(2) > a')
+    getalcontent_notice_locator=(By.CSS_SELECTOR,'body > div.container > div.alert.alert-success')
+    tingyong_notice_locator = (By.CSS_SELECTOR, '#form1 > table > tbody > tr:nth-child(1) > td:nth-child(7) > a:nth-child(1)')
+    gettingyong_notice_locator=(By.CSS_SELECTOR,'body > div.container > div.alert.alert-success')
+    checkbox_notice_locator = (By.CSS_SELECTOR, '#form1 > table > tbody > tr:nth-child(1) > td:nth-child(1) > input')
+    delete_notice_locator = (By.CSS_SELECTOR, '#delete')
+    serchcontent_notice_locator=(By.ID,'search')
+    serch_notice_locator=(By.CSS_SELECTOR,'#searchForm > li:nth-child(4) > button')
+    getdelete_notice_locator=(By.CSS_SELECTOR,'#form1 > table > tbody > tr > td')
+
+
+    def add_notice(self):
+        '''添加公告元素定位'''
+        self.find_element(self.add_notice_locator).click()
+
+    def getadd_notice(self):
+        '''断言，添加公告成功后的公告添加成功元素定位'''
+        self.find_element(self.getadd_notice_locator)
+        return self.find_element(self.getadd_notice_locator)
+
+    def see_notice(self):
+        '''对公告列表中的第一个公告标题元素定位进行查看'''
+        self.find_element(self.see_notice_locator).click()
+
+    def getalcontent_notice(self):
+        '''修改标题以后跳转页面中获取公告保存成功元素定位'''
+        self.find_element(self.getalcontent_notice_locator)
+        return self.find_element(self.getalcontent_notice_locator)
+
+    def tingyong_notice(self):
+        '''公告列表页面中第一个公告点击停用的元素定位'''
+        self.find_element(self.tingyong_notice_locator).click()
+
+    def gettingyong_notice(self):
+        '''断言，停用以后在获取当前提示的修改成功，已停用的文本元素定位'''
+        self.find_element(self.gettingyong_notice_locator)
+        return self.find_element(self.gettingyong_notice_locator)
+
+    def checkbox_notice(self):
+        '''选择第一条公告前面的单选框'''
+        self.find_element(self.checkbox_notice_locator).click()
+
+    def delete_notice(self):
+        '''对删除按钮元素定位'''
+        self.find_element(self.delete_notice_locator).click()
+        self.driver.switch_to.alert.accept()
+
+    def serchcontent_notice(self,sercontent):
+        '''搜索框元素定位'''
+        self.find_element(self.serchcontent_notice_locator).send_keys(sercontent)
+
+    def serch_notice(self):
+        '''搜索按钮元素定位'''
+        self.find_element(self.serch_notice_locator).click()
+
+
+    def getdelete_notice(self):
+        '''断言，删除该公告后再搜索该公告提示暂无数据文本元素定位'''
+        self.find_element(self.delete_notice_locator)
+        return self.find_element(self.delete_notice_locator)
+
+
+
+
+
+
+
+
+
+
+    def delete_mail(self):
+        '''批量操作下的删除元素定位'''
+        self.find_element(self.delete_mail_locator).click()
+        self.driver.switch_to.alert.accept()
+
+    def getdelete_mail(self):
+        '''断言，删除消息后的删除成功元素定位'''
+        self.find_element(self.getdelete_mail_locator)
+        return self.find_element(self.getdelete_mail_locator)
+
+
