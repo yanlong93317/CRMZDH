e986e88ebad9fa20b6515c27c9ae971d08ed0d50
374680231@qq.com <374680231@qq.com>
2020-12-31 15:50:27 +0800

    新用例的用例提交12/30 15:06

--- datas/tools.py
+++ datas/tools.py
@@ -50,7 +50,6 @@ def data_receivables_ex():
     return (e_list)
 
 
-print(data_receivables_ex()[0][:4])
 
 
 def data_knowledge_ex():
@@ -60,6 +59,7 @@ def data_knowledge_ex():
     for n_row in range(1, table.nrows):
         e_list.append(table.row_values(n_row))
     return (e_list)
+print(data_knowledge_ex()[1][3])
 
 
 def data_clue_ex():
@@ -98,16 +98,10 @@ def data_notice_ex():
     return (e_list)
 
 
-def data_receivables_ex():
-    data = xlrd.open_workbook("../datas/crm_casedata.xlsx")
-    table = data.sheet_by_name("receivables")
-    e_list = []
-    for n_row in range(1, table.nrows):
-        e_list.append(table.row_values(n_row))
-    return (e_list)
 
 
-print(data_receivables_ex()[0][:4])
+
+
 
 
 def data_systemsettings_ex():
