9ea7f5bffd5826f66e30795ed42faf04d8002921
374680231@qq.com <374680231@qq.com>
2020-12-31 10:26:58 +0800

    新用例的用例提交12/30 10:26

--- config/config.py
+++ config/config.py
@@ -5,4 +5,4 @@ Host = "http://192.168.1.179"
 BASE_PATH = os.path.dirname(__file__).strip("config")
 REPORT_PATH = os.path.join(BASE_PATH, 'report')
 CASS_PATH = os.path.join(BASE_PATH, 'cases')
-DATAS_PATH =os.path.join(BASE_PATH,'datas')
+DATAS_PATH = os.path.join(BASE_PATH, 'datas')
