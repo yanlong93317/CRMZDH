4b8cfebd61b69a4ef444f3ddd3e17a8654c763ae
374680231@qq.com <374680231@qq.com>
2020-12-31 10:52:19 +0800

    新用例的用例提交12/30 10:52

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 import os
 
-Host = "http://192.168.1.179"
+Host = "http://192.168.1.211/crm/index.php?m=user&a=login"
 
 BASE_PATH = os.path.dirname(__file__).strip("config")
 REPORT_PATH = os.path.join(BASE_PATH, 'report')
